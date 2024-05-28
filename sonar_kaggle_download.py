9206b82991f90f05bb0f9fa504115afdcd915be7
OmarHaroon01 <65035203+OmarHaroon01@users.noreply.github.com>
2024-05-29 01:39:09 +0600

    Update sonar_kaggle_download.py

--- sonar_kaggle_download.py
+++ sonar_kaggle_download.py
@@ -3,22 +3,57 @@ import requests
 from kaggle.api.kaggle_api_extended import KaggleApi
 from tqdm import tqdm
 
-def download_file(url, output_path):
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
-    with open(output_path, 'wb') as file, tqdm(
-        desc=os.path.basename(output_path),
-        total=total_size,
-        unit='B',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in response.iter_content(chunk_size=1024):
-            file.write(data)
-            bar.update(len(data))
+class MyKaggleApi(KaggleApi):
+    def kernels_output(self, kernel, path, force=False, quiet=True):
+            """ retrieve output for a specified kernel
+                Parameters
+                ==========
+                kernel: the kernel to output
+                path: the path to pull files to on the filesystem
+                force: if output already exists, force overwrite (default False)
+                quiet: suppress verbosity (default is True)
+            """
+            if kernel is None:
+                raise ValueError('A kernel must be specified')
+            if '/' in kernel:
+                self.validate_kernel_string(kernel)
+                kernel_url_list = kernel.split('/')
+                owner_slug = kernel_url_list[0]
+                kernel_slug = kernel_url_list[1]
+            else:
+                owner_slug = self.get_config_value(self.CONFIG_NAME_USER)
+                kernel_slug = kernel
+
+            if path is None:
+                target_dir = self.get_default_download_dir('kernels', owner_slug,
+                                                        kernel_slug, 'output')
+            else:
+                target_dir = path
+
+            if not os.path.exists(target_dir):
+                os.makedirs(target_dir)
+
+            if not os.path.isdir(target_dir):
+                raise ValueError(
+                    'You must specify a directory for the kernels output')
+
+            response = self.process_response(
+                self.kernel_output_with_http_info(owner_slug, kernel_slug))
+            outfiles = []
+            for item in response['files']:
+                outfile = os.path.join(target_dir, item['fileName'])
+                outfiles.append(outfile)
+                download_response = requests.get(item['url'])
+                if force or self.download_needed(item, outfile, quiet):
+                    os.makedirs(os.path.split(outfile)[0], exist_ok=True)
+                    with open(outfile, 'wb') as out:
+                        out.write(download_response.content)
+                    if not quiet:
+                        print('Output file downloaded to %s' % outfile)
+
 
 def download_kaggle_kernel_output(kernel_slug, path='sonar/'):
-    api = KaggleApi()
+    api = MyKaggleApi()
     api.authenticate()
 
     # Ensure the path exists
@@ -28,16 +63,11 @@ def download_kaggle_kernel_output(kernel_slug, path='sonar/'):
     # List output files for the specified kernel
     output_files = api.kernels_output(kernel_slug, path)
 
-    for file in output_files['files']:
-        file_name = file['name']
-        file_url = file['url']
-        output_path = os.path.join(path, file_name)
-        download_file(file_url, output_path)
-
 
 
 if __name__ == "__main__":
 
+    
     file_names = [
         'lamia1ieeeacc/s-0-20000',
         'merliahsummer/s-20k-50k',
@@ -122,4 +152,3 @@ if __name__ == "__main__":
         download_kaggle_kernel_output(file)
         cnt += 1
         print(f'File {cnt} done')
-        
